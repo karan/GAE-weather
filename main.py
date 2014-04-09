37ad6ce357a60b36b2604e8e89fb879917e495e1
Sudeep Acharya <sudeep611@gmail.com>
2014-04-09 20:11:49 +0545

    A little bit of front-end and error handling.

--- main.py
+++ main.py
@@ -3,31 +3,35 @@
 import webapp2
 from urllib2 import urlopen
 import json
-
-FORM = """
-<html>
-<head></head>
-<form method="get" action="/weather">
-<input type="text" cols="10" name="zip">
-<input type="submit" value="How's it?">
-</form>
-</html>
-"""
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(FORM)
+        template_values = {}
+        self.response.out.write(template.render("index.html", template_values))
+        
 
 class WeatherHandler(webapp2.RequestHandler):
     def get(self):
-        url = 'http://api.openweathermap.org/data/2.5/weather?q=%s' \
-              % self.request.get('zip')
-        data = json.load(urlopen(url))
-        self.response.write('<p>%s - %s</p>' % (data["weather"][0]["main"], data["weather"][0]["description"]))
-        self.response.write('<p>Temp: %d C</br>Humidity: %s</p>' % (data["main"]["temp"] - 273, data["main"]["humidity"]))
-        self.response.write('<p>%s - %s</p>' % (data["name"], data["sys"]["country"]))
+        try:
+            url = 'http://api.openweathermap.org/data/2.5/weather?q=%s' \
+                  % self.request.get('zip')
+            data = json.load(urlopen(url))
+
+            template_values = {'weather': data["weather"][0]["main"],
+                               'description': data["weather"][0]["description"],
+                               'temperature': data["main"]["temp"] - 273,
+                               'humidity': data["main"]["humidity"],
+                               'name': data["name"],
+                               'country': data["sys"]["country"]}
+            self.response.out.write(template.render("result.html", template_values))                   
+        except:
+            self.response.out.write(template.render("error.html", {}))    
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/weather', WeatherHandler)
 ], debug=True)
+
+
+
